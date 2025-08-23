689ec461156656f48d8c0cac0bf7433e637a03a5
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2025-08-23 08:46:42 +0600

    iterable of map

--- map2.js
+++ map2.js
@@ -2,7 +2,16 @@
 let setFruits=new Map()
 
   setFruits.set(1,'apple')
+  setFruits.set(2,'gili')
+  setFruits.set(3,'bobol')
+  setFruits.set(4,'voloib')
 
-  console.log(setFruits);
+  console.log(setFruits.values(2));
   console.log(setFruits.get(1));
+
   
+  for (const [key,value] of setFruits) {
+    console.log(key);
+    console.log(value);
+    
+  }
---
+++ obj2.js
@@ -0,0 +1,8 @@
+const obj={
+    a:'abid',
+    b:'shofique'
+}
+
+ const rs= Object.entries(obj)
+ console.log(rs);
+ 
